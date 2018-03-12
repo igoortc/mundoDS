556d6d762cef68479cd2803300fbfdb55a8a1bf0
igoortc <igoortc@gmail.com>
2018-03-12 15:30:50 -0300

    Force HTTPS

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -24,7 +25,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-
+        URL::forceScheme('https');
         parent::boot();
     }
 
