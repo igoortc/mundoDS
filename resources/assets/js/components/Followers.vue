cf7e2fad1b715ea7850bdd6ebd71268aaae468b6
igoortc <igoortc@gmail.com>
2018-03-16 21:42:28 -0300

    Fixing API fetching

--- resources/assets/js/components/Followers.vue
+++ resources/assets/js/components/Followers.vue
@@ -33,11 +33,11 @@
             getFollowers() {
                 let self = this;
                 let followers = [];
-                this.$http.get('/user/' + self.user + '/followers/')
+                this.axios.get('/api/user/' + self.user + '/followers/')
                     .then(function (response) {
                         followers = response.data.data;
                         for (let i = 0; i < followers.length; i++) {
-                            this.$http.get('/users/' + followers[i].user_id)
+                            this.axios.get('/api/users/' + followers[i].user_id)
                             .then(function (response) {
                                 self.followersInfo.push(response.data.data);
                                 self.count++;
