813f46f0f5d1e1f02e34f769b7bcc85c7e921076
igoortc <igoortc@gmail.com>
2018-06-09 02:12:59 -0300

    Pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_CLUSTER'),
-                'encrypted' => false,
+                'encrypted' => true,
             ],
         ],
 
